db2dc02d652be17fec21d5c0e9ea947ff6a7f7b3
Pedro <mendez.pedro94@gmail.com>
2021-05-28 22:45:18 -0400

    proyecto terminado

--- grafico.rb
+++ grafico.rb
@@ -1,32 +1,30 @@
 
-filter = [4,3,2,1]
-
 
 
 def chart(filter)
     x = 0
     filter.each do |d|
         print '|'
-        filter[x].times do |i|
-            i.times do |j|    
-                print '*'
-            end
+        filter[x].times do |i|  
+            print '**'
         end
         print "\n"
         x += 1
     end
 
-    puts(">-------------------------")
-    filter.length.times do |i|
+    print ">"
+    puts "--" * filter.max #.max imprime el mayor valor del arreglo
+    print " "
+    filter.max.times do |i|
         print i + 1
         print " "
         
     end
     print "\n"
-    filter
+    
 end
 
 
 
 
-chart(filter)
+
